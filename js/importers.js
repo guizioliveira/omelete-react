9420e73efb7e9a713eb7ca49fcf387b79c3477b7
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-03-25 02:07:51 -0300

    remove comments for importers js

--- js/importers.js
+++ js/importers.js
@@ -1,10 +1,7 @@
 export function doLoad(div, pages) {
     if( pages.length > 0) {
-        // use shift to get the next page off the array
         $.get(pages.shift(), function(content) {
-            // append content to .main
             $(`${div}`).append(content);
-            // call do load to get the next entry
             doLoad(div, pages);
         });
     }
